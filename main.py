77d3cb97c14cc0c020d469692ad8413798734377
fresmaa <muhammad.fresma26@gmail.com>
2023-02-26 10:50:18 +0700

    update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bot.botFacebook import *
 from bot.botTwitter import *
 from bot.botPinterest import *
 from bot.botTelegram import *
+from bot.botReplyTwitter import *
 
 # Twitter
 schedule.every(15).minutes.do(autoposting)
@@ -11,6 +12,9 @@ schedule.every(10).minutes.do(autoRetweetNonEleved)
 schedule.every(18).minutes.do(autoRepostNonEleved)
 schedule.every(25).minutes.do(autopostingAkunBackUp)
 schedule.every(20).minutes.do(autoRepostAkunAyah)
+
+# Reply Twitter
+schedule.every(30).minutes.do(posting)
 
 # Telegram
 schedule.every(30).minutes.do(autoPostingTelegram)

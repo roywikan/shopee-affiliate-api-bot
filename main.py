3546610c04e216df4ad9d5adc1b333244f30a8c5
fresmaa <muhammad.fresma26@gmail.com>
2023-03-19 17:17:57 +0700

    update

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ from bot.botReplyTwitter import *
 from bot.postVideoTwiiter import * 
 
 # Twitter
-# schedule.every(25).minutes.do(autoposting)
+schedule.every(25).minutes.do(autoposting)
 schedule.every(30).minutes.do(autoRetweetNonEleved)
-# schedule.every(18).minutes.do(autoRepostNonEleved)
+schedule.every(18).minutes.do(autoRepostNonEleved)
 schedule.every(25).minutes.do(autopostingAkunBackUp)
 # schedule.every(20).minutes.do(autoRepostAkunAyah)
 schedule.every(45).minutes.do(autopostingTrendingTopik)
 
 # Twitter - post video
-# schedule.every(200).minutes.do(postingVideo)
+schedule.every(100).minutes.do(postingVideo)
 
 # Reply Twitter
 # schedule.every(30).minutes.do(posting)

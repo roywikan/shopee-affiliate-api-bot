c4fcd81a9788d8dd99e495170c287cbc0e9dfb99
fresmaa <muhammad.fresma26@gmail.com>
2023-03-26 17:54:39 +0700

    update

--- bot/botPinterest.py
+++ bot/botPinterest.py
@@ -4,10 +4,10 @@ import mysql.connector
 
 # Connect Database
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="shopee_aff"
+  host="biofresma.my.id",
+  user="biofresm_shopee_aff",
+  password="Azzukhruf26",
+  database="biofresm_shopee_aff"
 )
 
 # Login Pinters
